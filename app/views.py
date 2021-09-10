fc3ebdc909b9d5d8ea4cb0f7f85bec9d915c9bb3
Kipkorir2017 <pkobenj41@gmail.com>
2021-09-10 17:43:11 +0300

    add title to the index file

--- app/views.py
+++ app/views.py
@@ -9,4 +9,6 @@ def index():
     View root page function that returns the index page and its data
 
     '''
+    title='Home-Welcome to The best news source site online'
+
     return render_template('index.html')
