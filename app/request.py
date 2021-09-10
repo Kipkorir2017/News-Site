902db09b77807b961fc75262f19a59122f98a443
Kipkorir2017 <pkobenj41@gmail.com>
2021-09-10 22:50:14 +0300

    add a function to process results in the request file

--- app/request.py
+++ app/request.py
@@ -28,10 +28,33 @@ def getSource(category):
 
         sources_results=None
 
-        if getSource_response['sources']:
-            sources_results_list=getSource_response['sources']
-            sources_results=sources_results = process_sources(sources_results_list)
+        if getSource_response['results']:
+            sources_results_list=getSource_response['results']
+            sources_results=sources_results = process_results(sources_results_list)
 
     return sources_results
 
-# def process_sources(sources_list):
+def process_results(sources_list):
+    '''
+    Function that processes the source results and transform them to a list of Objects
+
+    Args:
+        sources_list: A list of dictionaries that contain sources details
+
+    Returns:
+        sources_results: A list of sources objects
+
+    '''
+    sources_results=[]
+    for sources_item in sources_list:
+        id=sources_item.get('id')
+        name = sources_item.get('name') 
+        description=sources_item.get('description')
+        url=sources_item.get('url')
+        category=sources_item.get('category') 
+        country=sources_item.get('country')
+
+        sources_objects= Source(id, name, description, url, category, country)
+        sources_results.append(sources_objects)
+        
+    return sources_results
